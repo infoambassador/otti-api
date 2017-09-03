20a433073b0ad6da36e474e546ab18e58ada8e8b
infoambassador <mlewis0260@gmail.com>
2017-09-03 16:55:55 -0500

    Testing environment set configuration.

--- app.py
+++ app.py
@@ -8,6 +8,7 @@ app.config.from_object(os.environ['APP_SETTINGS'])
 @app.route('/')
 def hello():
         return "Hello World!"
+        print(os.environ['APP_SETTINGS'])
 
 @app.route('/<name>')
 def hello_name(name):
