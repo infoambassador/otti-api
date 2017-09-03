8b28e9922ee1dd53d0ae56d6c4945448d1a4be1a
infoambassador <mlewis0260@gmail.com>
2017-09-03 16:58:06 -0500

    Fixed typo.

--- app.py
+++ app.py
@@ -7,8 +7,8 @@ app.config.from_object(os.environ['APP_SETTINGS'])
 
 @app.route('/')
 def hello():
-        return "Hello World!"
         print(os.environ['APP_SETTINGS'])
+        return "Hello World!"
 
 @app.route('/<name>')
 def hello_name(name):
