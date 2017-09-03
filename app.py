436e7bc36a0c0a3a0889696b09825e21332305eb
infoambassador <mlewis0260@gmail.com>
2017-09-03 16:34:37 -0500

    Fixed bug with name parameter route.

--- app.py
+++ app.py
@@ -7,8 +7,8 @@ def hello():
         return "Hello World!"
 
 @app.route('/<name>')
-def hello_name():
+def hello_name(name):
         return "Hello {}!".format(name)
 
 if __name__ == '__main__':
-        app.run()
+        app.run(debug=True)
